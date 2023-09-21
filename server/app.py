5049197c06ae51633fd88798ce02d9bd0f36d7a6
Briankipr <Brayos718@gmail.com>
2023-09-21 10:45:50 +0300

    assignment done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,20 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, Author, Post
+from model import Author, Post
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Change the URI as needed
+db = SQLAlchemy(app)
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -17,3 +30,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    
--- server/models.py
+++ server/models.py
@@ -1,32 +1,73 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import validates
-db = SQLAlchemy()
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from app import db  # Import db from your Flask app
 
 class Author(db.Model):
-    __tablename__ = 'authors'
-    # Add validations and constraints 
-
-    id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+    # ... (rest of your Author model definition)
 
 class Post(db.Model):
-    __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String)
-    category = db.Column(db.String)
-    summary = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
 
+    def __init__(self, title, content, author_id):
+        self.title = title
+        self.content = content
+        self.author_id = author_id
+    # Attributes and methods of the Post class go here
 
-    def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    # ... (rest of your Post model definition)
+
+    Base = declarative_base()
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    phone_number = Column(String(length=10), nullable=False)
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if len(value) != 10:
+            raise ValueError("Phone number must be exactly ten digits.")
+        return value
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    summary = Column(String(length=250))
+    category = Column(String, nullable=False)
+
+    @validates('title')
+    def validate_title(self, key, value):
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(keyword in value for keyword in clickbait_keywords):
+            raise ValueError("Title must be sufficiently clickbait-y.")
+        return value
+
+    @validates('content')
+    def validate_content(self, key, value):
+        if len(value) < 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either Fiction or Non-Fiction.")
+        return value
+
+# Create a SQLAlchemy engine, session, and perform migrations here
+engine = create_engine('sqlite:///mydatabase.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
